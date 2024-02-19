56ead5cc9329aacf486af93eccc1f8797697b27a
SamjanaK <40650340+SamjanaK@users.noreply.github.com>
2024-02-19 21:33:25 +0545

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("******Hello world!***********");
-          System.out.println("******Hello world Again and Again!***********");
+        Syste.out.println("******Hello world!***********");
+          Syste.out.println("******Hello world Again and Again!***********");
     }
 }
